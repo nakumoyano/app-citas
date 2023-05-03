61ca86d3f67f246d5616a193fc38ab20d7a51212
Nahuel Moyano <naku.24moyano@gmail.com>
2023-05-03 18:36:15 -0300

    listo todo

---
+++ src/app/components/crear-citas/crear-citas.component.ts
@@ -0,0 +1,52 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-citas',
+  templateUrl: './crear-citas.component.html',
+  styleUrls: ['./crear-citas.component.css'],
+})
+export class CrearCitasComponent implements OnInit {
+  @Output() nuevaCita = new EventEmitter<any>();
+
+  nombre = '';
+  fecha = '';
+  hora = '';
+  sintomas = '';
+
+  formularioIncorrecto = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  agregarCita() {
+    if (
+      this.nombre == '' ||
+      this.fecha == '' ||
+      this.hora == '' ||
+      this.sintomas == ''
+    ) {
+      this.formularioIncorrecto = true;
+      return;
+    }
+    this.formularioIncorrecto = false;
+
+    const CITA = {
+      nombre: this.nombre,
+      fecha: this.fecha,
+      hora: this.hora,
+      sintomas: this.sintomas,
+    };
+
+    this.nuevaCita.emit(CITA);
+
+    this.resetCampos();
+  }
+
+  resetCampos() {
+    this.nombre = '';
+    this.fecha = '';
+    this.hora = '';
+    this.sintomas = '';
+  }
+}
